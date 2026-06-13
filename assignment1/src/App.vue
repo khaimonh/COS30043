a82adf6e66f27ff8f00249d0dc1a0a4f930b7af1
matchacookie <tongkhaiminh@gmail.com>
2026-06-13 07:34:22 +0700

    update css

--- assignment1/src/App.vue
+++ assignment1/src/App.vue
@@ -14,21 +14,3 @@ import RegistrationForm from './components/RegistrationForm.vue'
         <RegistrationForm />
     </div>
 </template>
-
-<style scoped>
-.skip-link {
-    position: absolute;
-    top: -40px;
-    left: 0;
-    background: #3b82f6;
-    color: white;
-    padding: 8px 16px;
-    text-decoration: none;
-    border-radius: 4px;
-    z-index: 100;
-}
-
-.skip-link:focus {
-    top: 0;
-}
-</style>
