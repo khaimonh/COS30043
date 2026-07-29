079402e7e097983c2780559d727daf48f882ad97
matchacookie <tongkhaiminh@gmail.com>
2026-07-29 17:35:14 +0700

    setup get_current_user

--- final_project/backend/deps.py
+++ final_project/backend/deps.py
@@ -1,7 +1,21 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import select
 from database import SessionLocal
+from passlib.context import CryptContext
 from typing import Annotated
 from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+
+from models import User
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("AUTH_SECRET_KEY")
+ALGORITHM = os.getenv("AUTH_ALGORITHM")
 
 def get_db():
     db = SessionLocal()
@@ -10,3 +24,29 @@ def get_db():
     finally:
         db.close()
 db_dependency = Annotated[Session, Depends(get_db)]
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="/auth/token")
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)], db: db_dependency) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate user",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: int = payload.get("sub")
+        if user_id is None:
+            raise credentials_exception
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user")
+
+    user = db.scalar(select(User).where(User.user_id == user_id))
+
+    if user is None:
+        raise credentials_exception
+
+    return user
+user_dependency = Annotated[User, Depends(get_current_user)]
